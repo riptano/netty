d05d146b236b4c868284bf9a555c98c77edb39d4
Jake Luciani <jake@datastax.com>
2019-10-03 14:18:02 -0400

    force yes

--- docker-datastax-release.sh
+++ docker-datastax-release.sh
@@ -3,7 +3,7 @@
 if [ ! -f /usr/bin/docker ]; then
     export DEBIAN_FRONTEND=noninteractive
     sudo apt-get update
-    sudo apt-get install software-properties-common
+    sudo apt-get install -y software-properties-common
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
     sudo add-apt-repository \
         "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
